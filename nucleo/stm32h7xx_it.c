085f37669dc9083c83331165f9bec91162273b18
colinporth <colinporth@users.noreply.github.com>
2018-06-29 11:32:17 +0100

    first test of nucleo with jlink

--- nucleo/stm32h7xx_it.c
+++ nucleo/stm32h7xx_it.c
@@ -2,19 +2,11 @@
 #include "stm32h7xx_nucleo_144.h"
 
 void NMI_Handler() {}
-
-void HardFault_Handler() { while (1) {} }
-
 void MemManage_Handler() { while (1) { } }
-
 void BusFault_Handler() { while (1) { } }
-
 void UsageFault_Handler() { while (1) { } }
-
 void SVC_Handler() { }
-
 void DebugMon_Handler() { }
-
 void PendSV_Handler() { }
 
 void SysTick_Handler() {
