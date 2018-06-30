cbf2ac1e58aefda877caac1696d3ff528f9d5920
colinporth <colinporth@users.noreply.github.com>
2018-06-30 12:57:08 +0100

    ok

--- nucleo/main.cpp
+++ nucleo/main.cpp
@@ -3,7 +3,162 @@
 #include <stdlib.h>
 #include "stm32h7xx_hal.h"
 #include "stm32h7xx_nucleo_144.h"
+//{{{  defines
+#define SDRAM_OK     ((uint8_t)0x00)
+#define SDRAM_ERROR  ((uint8_t)0x01)
 
+#define SDRAM_DEVICE_ADDR  ((uint32_t)0xD0000000)
+#define SDRAM_DEVICE_SIZE  ((uint32_t)0x01000000)
+
+#define REFRESH_COUNT                    ((uint32_t)0x0603)   /* SDRAM refresh counter (100Mhz FMC clock) */
+#define SDRAM_TIMEOUT                    ((uint32_t)0xFFFF)
+
+#define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
+#define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
+#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
+#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
+#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
+
+#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
+#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
+
+#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
+#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
+//}}}
+
+//{{{
+void sdRamInit() {
+  //{{{  pins
+  //  FMC_SDCKE1  PB05
+  //  FMC_SDNE1   PB06
+  //
+  //  FMC_A0:A5   PF00:PF05
+  //  FMC_A6:A9   PF12:PF15
+  //  FMC_A10:A11 PG00:PG01
+  //  FMC_BA0:BA1 PG04:PG05
+  //
+  //  FMC_SDCLK   PG08
+  //  FMC_SDNCAS  PG15
+  //  FMC_SDNRAS  PF11
+  //  FMC_SDNWE   PC00
+  //
+  //  FMC_D0:D1   PD14:PD15
+  //  FMC_D2:D3   PD00:PD01
+  //  FMC_D4:D12  PE07:PE15
+  //  FMC_D13:D15 PD08:PD10
+  //
+  //  FMC_NBL0    PE00
+  //  FMC_NBL1    PE01
+  //}}}
+  //{{{  clocks
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+  __HAL_RCC_GPIOE_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
+  __HAL_RCC_FMC_CLK_ENABLE();
+  //}}}
+
+  /* Common GPIO configuration */
+  GPIO_InitTypeDef gpio_init_structure;
+  gpio_init_structure.Mode      = GPIO_MODE_AF_PP;
+  gpio_init_structure.Pull      = GPIO_PULLUP;
+  gpio_init_structure.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
+  gpio_init_structure.Alternate = GPIO_AF12_FMC;
+
+  gpio_init_structure.Pin   = GPIO_PIN_5 | GPIO_PIN_6;
+  HAL_GPIO_Init (GPIOB, &gpio_init_structure);
+
+  gpio_init_structure.Pin   = GPIO_PIN_0;
+  HAL_GPIO_Init (GPIOC, &gpio_init_structure);
+
+  gpio_init_structure.Pin   = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_8| GPIO_PIN_9 | GPIO_PIN_10 |
+                              GPIO_PIN_14 | GPIO_PIN_15;
+  HAL_GPIO_Init (GPIOD, &gpio_init_structure);
+
+  gpio_init_structure.Pin   = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 |
+                              GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+  HAL_GPIO_Init (GPIOE, &gpio_init_structure);
+
+  gpio_init_structure.Pin   = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2| GPIO_PIN_3 | GPIO_PIN_4 |
+                              GPIO_PIN_5 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
+  HAL_GPIO_Init (GPIOF, &gpio_init_structure);
+
+  gpio_init_structure.Pin   = GPIO_PIN_0 | GPIO_PIN_1 |GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_8 | GPIO_PIN_15;
+  HAL_GPIO_Init (GPIOG, &gpio_init_structure);
+
+  SDRAM_HandleTypeDef sdramHandle;
+  sdramHandle.Instance = FMC_SDRAM_DEVICE;
+
+  /* Timing configuration for 100Mhz as SDRAM clock frequency (System clock is up to 200Mhz) */
+  FMC_SDRAM_TimingTypeDef Timing;
+  Timing.LoadToActiveDelay    = 2;
+  Timing.ExitSelfRefreshDelay = 7;
+  Timing.SelfRefreshTime      = 4;
+  Timing.RowCycleDelay        = 7;
+  Timing.WriteRecoveryTime    = 2;
+  Timing.RPDelay              = 2;
+  Timing.RCDDelay             = 2;
+
+  sdramHandle.Init.SDBank             = FMC_SDRAM_BANK2;
+  sdramHandle.Init.SDClockPeriod      = FMC_SDRAM_CLOCK_PERIOD_3;
+  sdramHandle.Init.ReadBurst          = FMC_SDRAM_RBURST_ENABLE;
+  sdramHandle.Init.CASLatency         = FMC_SDRAM_CAS_LATENCY_3;
+
+  sdramHandle.Init.ColumnBitsNumber   = FMC_SDRAM_COLUMN_BITS_NUM_9;
+  sdramHandle.Init.RowBitsNumber      = FMC_SDRAM_ROW_BITS_NUM_12;
+  sdramHandle.Init.MemoryDataWidth    = FMC_SDRAM_MEM_BUS_WIDTH_16;
+  sdramHandle.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
+  sdramHandle.Init.WriteProtection    = FMC_SDRAM_WRITE_PROTECTION_DISABLE;
+  sdramHandle.Init.ReadPipeDelay      = FMC_SDRAM_RPIPE_DELAY_0;
+
+  if (HAL_SDRAM_Init (&sdramHandle, &Timing) != HAL_OK)
+    printf ("HAL_SDRAM_Init fialed\n");
+
+  __IO uint32_t tmpmrd = 0;
+
+  /* Step 1: Configure a clock configuration enable command */
+  FMC_SDRAM_CommandTypeDef Command;
+  Command.CommandMode            = FMC_SDRAM_CMD_CLK_ENABLE;
+  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2;
+  Command.AutoRefreshNumber      = 1;
+  Command.ModeRegisterDefinition = 0;
+  HAL_SDRAM_SendCommand (&sdramHandle, &Command, SDRAM_TIMEOUT);
+  HAL_Delay(1);
+
+  /* Step 3: Configure a PALL (precharge all) command */
+  Command.CommandMode            = FMC_SDRAM_CMD_PALL;
+  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2;
+  Command.AutoRefreshNumber      = 1;
+  Command.ModeRegisterDefinition = 0;
+  HAL_SDRAM_SendCommand(&sdramHandle, &Command, SDRAM_TIMEOUT);
+
+  /* Step 4: Configure an Auto Refresh command */
+  Command.CommandMode            = FMC_SDRAM_CMD_AUTOREFRESH_MODE;
+  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2;
+  Command.AutoRefreshNumber      = 8;
+  Command.ModeRegisterDefinition = 0;
+  HAL_SDRAM_SendCommand (&sdramHandle, &Command, SDRAM_TIMEOUT);
+
+  /* Step 5: Program the external memory mode register */
+  tmpmrd = (uint32_t)SDRAM_MODEREG_BURST_LENGTH_1          |
+                     SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL   |
+                     SDRAM_MODEREG_CAS_LATENCY_3           |
+                     SDRAM_MODEREG_OPERATING_MODE_STANDARD |
+                     SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;
+
+  Command.CommandMode            = FMC_SDRAM_CMD_LOAD_MODE;
+  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2;
+  Command.AutoRefreshNumber      = 1;
+  Command.ModeRegisterDefinition = tmpmrd;
+  HAL_SDRAM_SendCommand (&sdramHandle, &Command, SDRAM_TIMEOUT);
+
+  HAL_SDRAM_ProgramRefreshRate (&sdramHandle, REFRESH_COUNT);
+  }
+//}}}
 //{{{
 void systemClockConfig() {
 //   System Clock       = PLL (HSE BYPASS)
@@ -89,7 +244,7 @@ void sdRamTest (uint32_t fromValue, uint32_t toValue, uint16_t* addr, uint32_t l
         readOk++;
       else {
         if (readErr < 4)
-          printf ("- error %p %02x %d - r:%04x != %04x\n", readAddress, i, readErr, 
+          printf ("- error %p %02x %d - r:%04x != %04x\n", readAddress, i, readErr,
                                                            readWord1 & 0x00FF, (j+i) & 0x00FF);
         readErr++;
         }
@@ -102,191 +257,6 @@ void sdRamTest (uint32_t fromValue, uint32_t toValue, uint16_t* addr, uint32_t l
     //           hex (readErr) + " " +
     //           dec ((readOk * 100) / (len/2)));
     }
-  }
-//}}}
-
-//{{{  defines
-#define SDRAM_OK     ((uint8_t)0x00)
-#define SDRAM_ERROR  ((uint8_t)0x01)
-
-#define SDRAM_DEVICE_ADDR  ((uint32_t)0xD0000000)
-#define SDRAM_DEVICE_SIZE  ((uint32_t)0x1000000)  /* SDRAM device size in Bytes (32MB)*/
-
-#define SDRAM_MEMORY_WIDTH               FMC_SDRAM_MEM_BUS_WIDTH_32
-#define SDCLOCK_PERIOD                   FMC_SDRAM_CLOCK_PERIOD_3
-#define REFRESH_COUNT                    ((uint32_t)0x0603)   /* SDRAM refresh counter (100Mhz FMC clock) */
-#define SDRAM_TIMEOUT                    ((uint32_t)0xFFFF)
-
-#define SDRAM_MODEREG_BURST_LENGTH_1             ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_LENGTH_2             ((uint16_t)0x0001)
-#define SDRAM_MODEREG_BURST_LENGTH_4             ((uint16_t)0x0002)
-#define SDRAM_MODEREG_BURST_LENGTH_8             ((uint16_t)0x0004)
-#define SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL      ((uint16_t)0x0000)
-#define SDRAM_MODEREG_BURST_TYPE_INTERLEAVED     ((uint16_t)0x0008)
-
-#define SDRAM_MODEREG_CAS_LATENCY_2              ((uint16_t)0x0020)
-#define SDRAM_MODEREG_CAS_LATENCY_3              ((uint16_t)0x0030)
-
-#define SDRAM_MODEREG_OPERATING_MODE_STANDARD    ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_PROGRAMMED ((uint16_t)0x0000)
-#define SDRAM_MODEREG_WRITEBURST_MODE_SINGLE     ((uint16_t)0x0200)
-//}}}
-SDRAM_HandleTypeDef sdramHandle;
-static FMC_SDRAM_TimingTypeDef Timing;
-static FMC_SDRAM_CommandTypeDef Command;
-//{{{
-void gpioInit (SDRAM_HandleTypeDef* hsdram, void* Params) {
-//  FMC_SDCKE1  PB05
-//  FMC_SDNE1   PB06
-//
-//  FMC_A0:A5   PF00:PF05
-//  FMC_A6:A9   PF12:PF15
-//  FMC_A10:A11 PG00:PG01
-//  FMC_BA0:BA1 PG04:PG05
-//
-//  FMC_SDCLK   PG08
-//  FMC_SDNCAS  PG15
-//  FMC_SDNRAS  PF11
-//  FMC_SDNWE   PC00
-//
-//  FMC_D0:D1   PD14:PD15
-//  FMC_D2:D3   PD00:PD01
-//  FMC_D4:D12  PE07:PE15
-//  FMC_D13:D15 PD08:PD10
-//
-//  FMC_NBL0    PE00
-//  FMC_NBL1    PE01
-
-  __HAL_RCC_GPIOB_CLK_ENABLE();
-  __HAL_RCC_GPIOC_CLK_ENABLE();
-  __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOE_CLK_ENABLE();
-  __HAL_RCC_GPIOF_CLK_ENABLE();
-  __HAL_RCC_GPIOG_CLK_ENABLE();
-  __HAL_RCC_FMC_CLK_ENABLE();
-
-  /* Common GPIO configuration */
-  GPIO_InitTypeDef gpio_init_structure;
-  gpio_init_structure.Mode      = GPIO_MODE_AF_PP;
-  gpio_init_structure.Pull      = GPIO_PULLUP;
-  gpio_init_structure.Speed     = GPIO_SPEED_FREQ_VERY_HIGH;
-  gpio_init_structure.Alternate = GPIO_AF12_FMC;
-
-  gpio_init_structure.Pin   = GPIO_PIN_5 | GPIO_PIN_6;
-  HAL_GPIO_Init (GPIOB, &gpio_init_structure);
-
-  gpio_init_structure.Pin   = GPIO_PIN_0;
-  HAL_GPIO_Init (GPIOC, &gpio_init_structure);
-
-  gpio_init_structure.Pin   = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_8| GPIO_PIN_9 | GPIO_PIN_10 |
-                              GPIO_PIN_14 | GPIO_PIN_15;
-  HAL_GPIO_Init (GPIOD, &gpio_init_structure);
-
-  gpio_init_structure.Pin   = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_7 | GPIO_PIN_8 | GPIO_PIN_9 |
-                              GPIO_PIN_10 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-  HAL_GPIO_Init (GPIOE, &gpio_init_structure);
-
-  gpio_init_structure.Pin   = GPIO_PIN_0 | GPIO_PIN_1 | GPIO_PIN_2| GPIO_PIN_3 | GPIO_PIN_4 |
-                              GPIO_PIN_5 | GPIO_PIN_11 | GPIO_PIN_12 | GPIO_PIN_13 | GPIO_PIN_14 | GPIO_PIN_15;
-  HAL_GPIO_Init (GPIOF, &gpio_init_structure);
-
-  gpio_init_structure.Pin   = GPIO_PIN_0 | GPIO_PIN_1 |GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_8 | GPIO_PIN_15;
-  HAL_GPIO_Init (GPIOG, &gpio_init_structure);
-  }
-//}}}
-//{{{
-/**
-  * @brief  Programs the SDRAM device.
-  * @param  RefreshCount: SDRAM refresh counter value
-  * @retval None
-  */
-void initSequence (uint32_t RefreshCount) {
-
-  __IO uint32_t tmpmrd = 0;
-
-  /* Step 1: Configure a clock configuration enable command */
-  Command.CommandMode            = FMC_SDRAM_CMD_CLK_ENABLE;
-  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2;
-  Command.AutoRefreshNumber      = 1;
-  Command.ModeRegisterDefinition = 0;
-
-  /* Send the command */
-  HAL_SDRAM_SendCommand (&sdramHandle, &Command, SDRAM_TIMEOUT);
-
-  /* Step 2: Insert 100 us minimum delay */
-  /* Inserted delay is equal to 1 ms due to systick time base unit (ms) */
-  HAL_Delay(1);
-
-  /* Step 3: Configure a PALL (precharge all) command */
-  Command.CommandMode            = FMC_SDRAM_CMD_PALL;
-  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2;
-  Command.AutoRefreshNumber      = 1;
-  Command.ModeRegisterDefinition = 0;
-
-  /* Send the command */
-  HAL_SDRAM_SendCommand(&sdramHandle, &Command, SDRAM_TIMEOUT);
-
-  /* Step 4: Configure an Auto Refresh command */
-  Command.CommandMode            = FMC_SDRAM_CMD_AUTOREFRESH_MODE;
-  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2;
-  Command.AutoRefreshNumber      = 8;
-  Command.ModeRegisterDefinition = 0;
-
-  /* Send the command */
-  HAL_SDRAM_SendCommand (&sdramHandle, &Command, SDRAM_TIMEOUT);
-
-  /* Step 5: Program the external memory mode register */
-  tmpmrd = (uint32_t)SDRAM_MODEREG_BURST_LENGTH_1          |
-                     SDRAM_MODEREG_BURST_TYPE_SEQUENTIAL   |
-                     SDRAM_MODEREG_CAS_LATENCY_3           |
-                     SDRAM_MODEREG_OPERATING_MODE_STANDARD |
-                     SDRAM_MODEREG_WRITEBURST_MODE_SINGLE;
-
-  Command.CommandMode            = FMC_SDRAM_CMD_LOAD_MODE;
-  Command.CommandTarget          = FMC_SDRAM_CMD_TARGET_BANK2;
-  Command.AutoRefreshNumber      = 1;
-  Command.ModeRegisterDefinition = tmpmrd;
-
-  /* Send the command */
-  HAL_SDRAM_SendCommand (&sdramHandle, &Command, SDRAM_TIMEOUT);
-
-  /* Step 6: Set the refresh rate counter */
-  /* Set the device refresh rate */
-  HAL_SDRAM_ProgramRefreshRate (&sdramHandle, RefreshCount);
-  }
-//}}}
-//{{{
-void sdRamInit() {
-
-  gpioInit (&sdramHandle, NULL);
-
-  sdramHandle.Instance = FMC_SDRAM_DEVICE;
-
-  /* Timing configuration for 100Mhz as SDRAM clock frequency (System clock is up to 200Mhz) */
-  Timing.LoadToActiveDelay    = 2;
-  Timing.ExitSelfRefreshDelay = 7;
-  Timing.SelfRefreshTime      = 4;
-  Timing.RowCycleDelay        = 7;
-  Timing.WriteRecoveryTime    = 2;
-  Timing.RPDelay              = 2;
-  Timing.RCDDelay             = 2;
-
-  sdramHandle.Init.SDBank             = FMC_SDRAM_BANK2;
-  sdramHandle.Init.SDClockPeriod      = FMC_SDRAM_CLOCK_PERIOD_3;
-  sdramHandle.Init.ReadBurst          = FMC_SDRAM_RBURST_ENABLE;
-  sdramHandle.Init.CASLatency         = FMC_SDRAM_CAS_LATENCY_3;
-
-  sdramHandle.Init.ColumnBitsNumber   = FMC_SDRAM_COLUMN_BITS_NUM_9;
-  sdramHandle.Init.RowBitsNumber      = FMC_SDRAM_ROW_BITS_NUM_12;
-  sdramHandle.Init.MemoryDataWidth    = FMC_SDRAM_MEM_BUS_WIDTH_16;
-  sdramHandle.Init.InternalBankNumber = FMC_SDRAM_INTERN_BANKS_NUM_4;
-  sdramHandle.Init.WriteProtection    = FMC_SDRAM_WRITE_PROTECTION_DISABLE;
-  sdramHandle.Init.ReadPipeDelay      = FMC_SDRAM_RPIPE_DELAY_0;
-
-  if (HAL_SDRAM_Init (&sdramHandle, &Timing) != HAL_OK)
-    printf ("HAL_SDRAM_Init fialed\n");
-
-  initSequence (REFRESH_COUNT);
   }
 //}}}
 
@@ -312,7 +282,7 @@ int main() {
     BSP_LED_Toggle (LED_BLUE);
     HAL_Delay (100);
     BSP_LED_Toggle (LED_RED);
-    sdRamTest (i, i, (uint16_t*)0xD0000000, 0x00800000);
+    sdRamTest (i, i, (uint16_t*)0xD0000000, 0x01000000);
     }
 
   return 0;
